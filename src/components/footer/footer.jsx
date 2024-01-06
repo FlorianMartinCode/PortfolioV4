ac57a3aaa0ed59d61ec998370650dda45756f8cb
Martin <florian.martin700@gmail.com>
2024-01-07 00:03:45 +0100

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Cv from '../../assets/cv-florian-martin.pdf'
 
 function footer() {
   return (
-    <footer>
-      
+    <footer className='footer-content' >
+      <Link to={Cv} target="_blank" download className='btn-about'>
+        <span>Télécharger le CV</span>
+      </Link>
+      <p className='copyright'>© 2024 Florian MARTIN. Tous droits réservés.</p>
     </footer>
   )
 }
