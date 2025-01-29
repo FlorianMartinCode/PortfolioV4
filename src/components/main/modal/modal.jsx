337a65940ed0bacf67b032a09343148cb556f3b9
Flo700 <florian.martin700@gmail.com>
2025-01-29 22:01:25 +0100

    Add button in modal

--- src/components/main/modal/modal.jsx
+++ src/components/main/modal/modal.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'; // Importation des icônes de flèche
 
 function Modal({ project, onClose, onPrevious, onNext }) {
   useEffect(() => {
@@ -15,15 +16,19 @@ function Modal({ project, onClose, onPrevious, onNext }) {
     };
   }, [onClose]);
 
+  const handleModalClick = (e) => {
+    e.stopPropagation(); // Empêche la propagation de l'événement de clic pour fermer la modal
+  };
+
   return (
     <div className="modal" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button onClick={onClose} className="close-button">
+      <div className="modal-content" onClick={handleModalClick}>
+        <button onClick={onClose} className="close-button" aria-label="Fermer la modal">
           ✖
         </button>
-        <div className='modal-centre'>
+        <div className="modal-centre">
           <img src={project.cover} alt={project.alt} />
-          <div className='desc-compétences'>
+          <div className="desc-compétences">
             <div>
               <h2>Client</h2>
               <p>{project.title}</p>
@@ -34,12 +39,20 @@ function Modal({ project, onClose, onPrevious, onNext }) {
             </div>
             <div className="compétences">
               {project.compétences.map((compétence) => (
-                <img key={compétence.id} src={compétence.logo} alt={`compétences ${compétence.id}`} />
+                <img key={compétence.id} src={compétence.logo} alt={`compétence ${compétence.id}`} />
               ))}
             </div>
           </div>
         </div>
         <div className="modal-links">
+          <button 
+            onClick={onPrevious} 
+            className="arrow-button left-arrow" 
+            aria-label="Projet précédent"
+          >
+            <FaArrowLeft />
+          </button>
+
           {project.urlSite && (
             <a href={project.urlSite} target="_blank" rel="noopener noreferrer">
               Voir le site
@@ -50,6 +63,14 @@ function Modal({ project, onClose, onPrevious, onNext }) {
               Voir sur GitHub
             </a>
           )}
+          
+          <button 
+            onClick={onNext} 
+            className="arrow-button right-arrow" 
+            aria-label="Projet suivant"
+          >
+            <FaArrowRight />
+          </button>
         </div>
       </div>
     </div>
